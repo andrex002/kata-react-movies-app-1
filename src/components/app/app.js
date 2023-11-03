97eee6d51366cecbb01a50e2ba4f9db19b213c50
andrex002 <sergeev-002@yandex.ru>
2023-11-03 23:58:05 +0300

    Выполнил задание 2.2.14

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Spin, Alert } from 'antd';
+import { Offline, Online } from 'react-detect-offline';
 
 import PageHeader from '../page-header/page-header';
 import SearchInput from '../search-input/search-input';
@@ -14,27 +16,64 @@ export default class App extends React.Component {
     movies: [],
     totalFilms: null,
     page: 1,
+    loading: true,
+    error: false,
   };
 
   componentDidMount() {
-    api.getAllMovies().then((response) => {
-      this.setState({
-        movies: response.results,
-        totalFilms: response.total_results,
-        page: response.page,
-      });
-    });
+    api
+      .getAllMovies()
+      .then((response) => {
+        this.setState({
+          movies: response.results,
+          totalFilms: response.total_results,
+          page: response.page,
+          loading: false,
+        });
+      })
+      .catch(this.onError);
   }
 
+  onError = () => {
+    this.setState({
+      error: true,
+      loading: false,
+    });
+  };
+
   render() {
+    const spinner = this.state.loading ? <Spin size={'large'} /> : null;
+    const content = !(this.state.loading || this.state.error) ? <MoviesList films={this.state.movies} /> : null;
+    const errorMessage = this.state.error ? (
+      <Alert
+        message="Server Error"
+        description="Something went wrong... Please try again later."
+        type="error"
+        showIcon
+      />
+    ) : null;
+
     return (
       <div className="container">
-        <PageHeader />
-        <main>
-          <SearchInput />
-          <MoviesList films={this.state.movies} />
-        </main>
-        <PageFooter totalFilms={this.state.totalFilms} page={this.state.page} />
+        <Online>
+          <PageHeader />
+          <main>
+            <SearchInput />
+            {errorMessage}
+            {spinner}
+            {content}
+          </main>
+          <PageFooter totalFilms={this.state.totalFilms} page={this.state.page} />
+        </Online>
+        <Offline>
+          <Alert
+            className="offline-message"
+            message="Offline"
+            description="Please check your Internet Connection."
+            type="error"
+            showIcon
+          />
+        </Offline>
       </div>
     );
   }
--- src/components/movies-item/movies-item.js
+++ src/components/movies-item/movies-item.js
@@ -3,7 +3,7 @@ import { Card, Flex, Typography, Tag, Space, Rate, Progress } from 'antd';
 import { format } from 'date-fns';
 import './movies-item.css';
 
-const еruncatesDescription = (text, clip = 200) => {
+const truncatesDescription = (text, clip = 200) => {
   text = text.trim();
   if (text.length <= clip) {
     return text;
@@ -13,8 +13,7 @@ const еruncatesDescription = (text, clip = 200) => {
   return `${croppedText}...`;
 };
 
-const MoviesItem = ({ film }) => {
-  //   console.log(film);
+const MoviesItemView = ({ film }) => {
   const releaseDate = format(new Date(film.release_date), 'MMMM d, y');
   const calculatesСolorRatingBar = () => {
     if (film.vote_average >= 7) {
@@ -27,35 +26,47 @@ const MoviesItem = ({ film }) => {
   };
 
   return (
+    <React.Fragment>
+      <img
+        className="card-film__poster"
+        src={`https://image.tmdb.org/t/p/original${film.poster_path}`}
+        alt={film.title}
+      />
+      <div className="card-film__info">
+        <Typography.Title className="card-film__title">{film.title}</Typography.Title>
+        <Typography.Text className="card-film__release-date" type="secondary">
+          {releaseDate}
+        </Typography.Text>
+        <Space className="card-film__genres" size={[0, 8]} wrap>
+          <Tag className="card-film__genre">Action</Tag>
+          <Tag className="card-film__genre">Drama</Tag>
+        </Space>
+        <Typography.Paragraph className="card-film__description">
+          {truncatesDescription(film.overview)}
+        </Typography.Paragraph>
+        <Rate className="card-film__rate" count={10}></Rate>
+        <Progress
+          className="card-film__vote-average"
+          type="circle"
+          percent={film.vote_average * 10}
+          format={() => Math.round(film.vote_average * 10) / 10}
+          size={34}
+          strokeColor={calculatesСolorRatingBar()}
+        />
+      </div>
+    </React.Fragment>
+  );
+};
+
+const MoviesItem = ({ film }) => {
+  //   console.log(film);
+  // const spinner = loading ? <Spin /> : null;
+  // const content = !loading ? <MoviesItemView film={film} /> : null;
+
+  return (
     <Card hoverable className="card-film">
       <Flex className="card-film__flex">
-        <img
-          className="card-film__poster"
-          src={`https://image.tmdb.org/t/p/original${film.poster_path}`}
-          alt={film.title}
-        />
-        <div className="card-film__info">
-          <Typography.Title className="card-film__title">{film.title}</Typography.Title>
-          <Typography.Text className="card-film__release-date" type="secondary">
-            {releaseDate}
-          </Typography.Text>
-          <Space className="card-film__genres" size={[0, 8]} wrap>
-            <Tag className="card-film__genre">Action</Tag>
-            <Tag className="card-film__genre">Drama</Tag>
-          </Space>
-          <Typography.Paragraph className="card-film__description">
-            {еruncatesDescription(film.overview)}
-          </Typography.Paragraph>
-          <Rate className="card-film__rate" count={10}></Rate>
-          <Progress
-            className="card-film__vote-average"
-            type="circle"
-            percent={film.vote_average * 10}
-            format={() => Math.round(film.vote_average * 10) / 10}
-            size={34}
-            strokeColor={calculatesСolorRatingBar()}
-          />
-        </div>
+        <MoviesItemView film={film} />
       </Flex>
     </Card>
   );
